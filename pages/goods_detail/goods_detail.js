92fad16bb2c7a62ac9d46cec8876e5167b259c63
黑猫 <sundream@163.com>
2019-08-04 05:24:50 +0800

    完成商品详情页面的动态渲染

--- pages/goods_detail/goods_detail.js
+++ pages/goods_detail/goods_detail.js
@@ -1,66 +1,46 @@
-// pages/goods_detail/goods_detail.js
+// 引入封装好的请求方法
+import {request} from "../../request/request.js";
+// 引入库 使小程序支持es7的async语法
+import regeneratorRuntime from '../../lib/runtime/runtime';
+//Page Object
 Page({
-
-  /**
-   * 页面的初始数据
-   */
-  data: {
-
+  data:{
+    // 商品详情数据s
+    goodsDetail:{}
   },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
+  // 商品id
+  goods_id:0,
+  onLoad(options){
+    // console.log(options);
+    this.goods_id=options.goods_id;
+    this.getGoodsDetail();
 
   }
-})
+  ,
+  // 获取商品详情数据
+  async getGoodsDetail(){
+    const res=await request({url:"/goods/detail",data:{goods_id:this.goods_id}})
+    // console.log(res);
+    let {goods_name,goods_price,goods_introduce,pics}=res.message;
+    this.setData({
+      goodsDetail:{goods_name,goods_price,goods_introduce,pics}
+    })
+  }
+  ,
+  // 点击展示轮播图大图
+  handlePreviewImage(e){
+    // console.log(e);
+    const {id}=e.currentTarget.dataset;
+    let urls=this.data.goodsDetail.pics.map(v=>{
+      return v.pics_big
+    });
+    let current=urls[id];
+    // console.log(id,urls,current)
+    wx.previewImage({
+      current, // 当前显示图片的http链接
+      urls// 需要预览的图片http链接列表
+    })
+  }
+  ,
+});
+  
