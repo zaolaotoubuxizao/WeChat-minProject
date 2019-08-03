ebe6fd35f5303434bbe4dbad30210f0a101f58ac
黑猫 <sundream@163.com>
2019-08-04 02:03:51 +0800

    完成商品列表页面的下拉刷新和全局等待效果

--- pages/goods_list/goods_list.js
+++ pages/goods_list/goods_list.js
@@ -1,76 +1,115 @@
 // 引入封装好的请求方法
-import { request } from "../../request/request.js";
+import {request} from "../../request/request.js";
+import regeneratorRuntime from '../../lib/runtime/runtime';
 Page({
-  data:{
+  data: {
     // tabs栏的数据
-    tabsData1:[
-      { id: 0, title: "综合", isActive: true },
-      { id: 1, title: "销量", isActive: false },
-      { id: 2, title: "价格", isActive: false }
+    tabsData1: [{
+        id: 0,
+        title: "综合",
+        isActive: true
+      },
+      {
+        id: 1,
+        title: "销量",
+        isActive: false
+      },
+      {
+        id: 2,
+        title: "价格",
+        isActive: false
+      }
     ],
     // 商品列表的数据
-    goodsList:[]
+    goodsList: []
   },
   // 请求商品列表数据所需参数
-  queryParam:{
-    query:"",
-    cid:"",
-    pagenum:1,
-    pagesize:10,
+  queryParam: {
+    query: "",
+    cid: "",
+    pagenum: 1,
+    pagesize: 10,
   },
   // 总的页数
-  totalPages:1,
+  totalPages: 1,
+  // 下拉刷新防御性变量
+  flag: false,
   // 生命周期函数，监听页面加载
-  onLoad(options){
+  onLoad(options) {
     // console.log(options);
-    this.queryParam.cid=options.cid
+    this.queryParam.cid = options.cid
     this.getGoodsList()
   },
   // 获取商品列表数据
-  getGoodsList(){
+  getGoodsList() {
     request({
-      url:"/goods/search",
-      data:this.queryParam
-    })
-      .then(res=>{
-        console.log(res);
+        url: "/goods/search",
+        data: this.queryParam
+      })
+      .then(res => {
+        // console.log(res);
         // 计算总的页数
-        this.totalPages=Math.ceil(res.message.total/this.queryParam.pagesize) 
+        this.totalPages = Math.ceil(res.message.total / this.queryParam.pagesize)
         // 要拼接数组
         this.setData({
-          goodsList:[...this.data.goodsList,...res.message.goods]
+          goodsList: [...this.data.goodsList, ...res.message.goods]
         })
+        // 成功拿回数据后，判断操作是否是由下刷新触发，然后关闭下拉刷新
+        if (this.flag) {
+          // console.log("是下拉刷新触发");
+          // 关闭下拉刷新
+          wx.stopPullDownRefresh()
+          // 再将防御性变量初始化
+          this.flag = false;
+        }
       })
+
   },
   // 页面上拉 滚动条触底事件
-  onReachBottom(){
+  onReachBottom() {
     // 当触底时当前页面+1
     // queryParam:{
     //   query:"",
     //   cid:"",
     //   pagenum:1,
     //   pagesize:10,
-    if(this.queryParam.pagenum>=this.totalPages){
+    if (this.queryParam.pagenum >= this.totalPages) {
       // 没有下一页数据
       // console.log("没有下一页数据");
       // Toast 会自动显示的提示框
       wx.showToast({
         title: '没有下一页数据',
         icon: 'none',
-      });  
-    }else{
+      });
+    } else {
       this.queryParam.pagenum++;
       this.getGoodsList()
     }
   },
+  // 下拉刷新重置页面
+  onPullDownRefresh: function () {
+    // 只有当下拉刷新给防御性变量赋值，再判断变量是否关闭下拉刷新
+    this.flag = true;
+    // 将页数 和 初始数据数组重置
+    this.queryParam.pagenum = 1;
+    this.setData({
+      goodsList: []
+    })
+    // 再调用请求数据方法载入初始数据
+    this.getGoodsList()
+  },
   // 子组件触发的事件 获取子组件tab选中的tab的id值
-  handleItemChange(active){
+  handleItemChange(active) {
     // console.log(active);
-    const {id}=active.detail;
-    let {tabsData1}=this.data
+    const {
+      id
+    } = active.detail;
+    let {
+      tabsData1
+    } = this.data
     // 修改当前类，并重置data中的数据
     tabsData1.forEach(v => {
-      v.id===id?v.isActive=true:v.isActive=false
+      v.id === id ? v.isActive = true : v.isActive = false
     });
     this.setData({
       tabsData1
--- request/request.js
+++ request/request.js
@@ -1,4 +1,12 @@
+// 申明一个初始变量，每次调用++，请求编译完--，判断是否为零时，则所有异步请求完成，关闭提示框
+let ajaxTimes=0;
 export const request=(requestData)=>{
+    // 调用即发送请求前显示loading提示框
+    ++ajaxTimes;
+    // console.log("请求第"+ajaxTimes+"次")
+    wx.showLoading({
+        title: '加载中',
+      })
     const baseUrl="https://api.zbztb.cn/api/public/v1"
     return new Promise((resolve,reject)=>{
         wx.request({...requestData,url:baseUrl+requestData.url,
@@ -7,6 +15,15 @@ export const request=(requestData)=>{
         },
         fail:(err)=>{
             reject(err)
+        },
+        // 接口调用结束的回调函数（调用成功、失败都会执行）	
+        complete(){
+            --ajaxTimes;
+            // console.log(ajaxTimes)
+            // 当变量为零时，则所有异步请求完成，关闭提示框
+            if(ajaxTimes===0){
+                wx.hideLoading();  
+            }
         }
         })
     })
